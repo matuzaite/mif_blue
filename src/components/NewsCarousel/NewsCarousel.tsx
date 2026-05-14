f711d7702c3dc8edd0b21d68f707ad7ea56dbd42
matuzaite <matuzaiteausteja@gmail.com>
2026-05-14 13:43:48 +0300

    fixed interval of slides change

--- src/components/NewsCarousel/NewsCarousel.tsx
+++ src/components/NewsCarousel/NewsCarousel.tsx
@@ -48,7 +48,7 @@ export default function NewsCarousel({ initialItems }: NewsCarouselProps) {
     if (autoRotateTimerRef.current) clearInterval(autoRotateTimerRef.current);
     autoRotateTimerRef.current = setInterval(() => {
       setCurrentIndex(prev => (prev + 1) % items.length);
-    }, 25000); // Increased interval to 25 seconds
+    }, 40000); // Increased interval to 40 seconds
   }, [items.length]);
 
   useEffect(() => {
