ceb92b41907e9f0b8ff21f1cb406ba47c4280aeb
matuzaite <matuzaiteausteja@gmail.com>
2026-06-15 11:30:51 +0300

    removed time and date

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,20 +1,9 @@
-'use client';
-import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from './Sidebar.module.scss';
 
 export default function Sidebar() {
-  const [time, setTime] = useState<Date | null>(null);
-
-  useEffect(() => {
-    setTime(new Date());
-    const interval = setInterval(() => setTime(new Date()), 1000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <aside className={styles.sidebar}>
-      {/* Logo */}
       <div className={styles.logoContainer}>
         <Image
           src="/VU_MIF_herbai.png"
@@ -25,17 +14,6 @@ export default function Sidebar() {
           style={{ filter: 'brightness(0) invert(1)' }}
         />
       </div>
-
-      {/* Live Clock */}
-      <div className={styles.clockSection}>
-        <p className={styles.time}>
-          {time ? time.toLocaleTimeString('lt-LT', { hour: '2-digit', minute: '2-digit' }) : '--:--'}
-        </p>
-        <p className={styles.date}>
-          {time ? time.toLocaleDateString('lt-LT', { day: 'numeric', month: 'long' }) : '...'}
-        </p>
-      </div>
-
     </aside>
   );
 }
